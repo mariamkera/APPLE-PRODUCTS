3547ae5e8d1019dbd6e5663acc8284df428170fa
russa88 <rusobulbulashvili76@gmail.com>
2024-09-26 18:18:16 +0400

    APPLE

---
+++ js/scrips.js
@@ -0,0 +1,43 @@
+function show1(){
+    let open1= document.getElementById("gadasasvleliN1").style.display; 
+    if(open1=="block"){
+        document.getElementById("gadasasvleliN1").style.display="none";
+    }
+    else{
+        document.getElementById("gadasasvleliN1").style.display="block";
+    }
+    
+}
+
+function show2(){
+    let open1= document.getElementById("gadasasvleliN2").style.display; 
+    if(open1=="block"){
+        document.getElementById("gadasasvleliN2").style.display="none";
+    }
+    else{
+        document.getElementById("gadasasvleliN2").style.display="block";
+    }
+    
+}
+
+function show3(){
+    let open1= document.getElementById("gadasasvleliN3").style.display; 
+    if(open1=="block"){
+        document.getElementById("gadasasvleliN3").style.display="none";
+    }
+    else{
+        document.getElementById("gadasasvleliN3").style.display="block";
+    }
+    
+}
+
+function show4(){
+    let open1= document.getElementById("gadasasvleliN4").style.display; 
+    if(open1=="block"){
+        document.getElementById("gadasasvleliN4").style.display="none";
+    }
+    else{
+        document.getElementById("gadasasvleliN4").style.display="block";
+    }
+    
+}
